b79d82af6d813c0da7854cbc02967938f14fdfdc
ivan rouskov <rouskoi@live.com>
2018-05-08 18:36:29 -0700

    Add files via upload

--- screens/DataOrganizer.js
+++ screens/DataOrganizer.js
@@ -14,34 +14,36 @@ import { WebBrowser } from 'expo';
 import GameScreen from '../screens/GameScreen';
 import SavedGamesScreen from '../screens/SavedGamesScreen';
 
-export default class DataOrganizer extends React.Component{
+ export default class DataOrganizer extends React.Component{
     //an array of DataOrgaznizers should be returned when game complete.
 
    constructor(HandData, isBigBlind, isSmallBlind){
        super(HandData, isBigBlind, isSmallBlind)
        {
-            var HandData = [];
-            let isBigBlind = null;
-            let isSmallBlind = null;
+            var HandData = HandData;
+            var isBigBlind = null;
+            var isSmallBlind = null;
        }
 
    }
-   buildArray(numberOfactions){
-       for(var i = 0; i < HandData.Array.length ; i++){
-       Array.prototype.HandData[i] = numberOfactions;
-       }
-   }
 
-     showHand(HandData, isBigBlind, isSmallBlind) {
+  // buildArray(numberOfactions){
+    //   for(var i = 0; i < HandData.Array.length ; i++){
+    //   Array.prototype.HandData[i] = numberOfactions;
+    //   }
+   //}
+
+/*       showHand() {
        
     var handstring = ''
-    for(i = 0; i <= HandData.length() ; i++){
-
+    for(i = 0; i < this.HandData.length ; i++){
+     handstring += this.HandData[i] + ' ' ;
     }
     return handstring;
+   }  */
+   showOnConsole() {
+   console.log(this.HandData + ' ' + this.isBigBlind + ' ' + this.isSmallBlind); //get to display correctly
    }
 
-   
 
-
-}
+};
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text, TouchableHighlight, } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, TouchableHighlight, Switch } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import DataOganizer from '../screens/DataOrganizer';
+import DataOrganizer from '../screens/DataOrganizer';
 
 export default class GamesScreen extends React.Component {
   static navigationOptions = {
@@ -9,9 +10,37 @@ export default class GamesScreen extends React.Component {
   };
 constructor(props) {
    super(props)
-   this.state = { check: 0, bet: 0, call: 0, raise: 0, reRaise: 0, fold: 0,  handNumber : 1, isHandinPlay : true }
+   this.state = { check: 0, bet: 0, call: 0, raise: 0, reRaise: 0, fold: 0,  handNumber : 1, isHandinPlay : true, isBigBlind : false, isSmallBlind : false, } 
    
  }
+ _handleToggleSwitch = () => this.setState(state => ({
+  isBigBlind: !state.isBigBlind}));
+  newHand(){
+      var HandArray = [];
+      
+        HandArray.push(this.state.check);
+        HandArray.push(this.state.bet);
+        HandArray.push(this.state.call);
+        HandArray.push(this.state.raise);
+        HandArray.push(this.state.reRaise);
+        HandArray.push(this.state.handNumber);
+        var Hand = new DataOrganizer(HandArray, this.isBigBlind, this.isSmallBlind); //saves to DataOrganizer class.
+        Hand.showOnConsole();
+        if(this.isBigBlind == true){
+          this.setState({isBigBlind: this.state.isBigBlind = false,isSmallBlind: this.state.isSmallBlind = true})
+          onValueChange= {isBigBlind : this._handleToggleSwitch} //why wont switch go back to normal?
+        }
+        else (this.isSmallBlind == true)
+        {
+          this.setState({isSmallBlind: this.state.isSmallBlind = false});
+        }
+
+       this.setState({handNumber: this.state.handNumber+1, check: this.state.check=0,  bet: this.state.bet=0, 
+        call: this.state.call=0,  raise: this.state.raise=0,  reRaise: this.state.reRaise=0, fold: this.state.fold=0})
+        //Clears data after each hand, needs to also be sent to DataOganizer, so may need to change this.})
+      }
+      
+ 
 //think about wrapping in while loop, while(handInPlay == true || if False clear and start new hand)
 //Once new hand is pressed, the data should be sent to DataOrganizer class to be prepared for storage.
 //Still need to add a Big Blind button.
@@ -38,14 +67,16 @@ constructor(props) {
    }
    else if(action == 'fold'){
      this.setState({fold: this.state.fold+1})
+     this.newHand(); //should also clear and start new hand
    }
-   else if(action == 'New Hand'){
+  else if(action == 'New Hand'){
      this.setState({handNumber: this.state.handNumber+1, /*DataOganizer:create(this.state.check),*/ check: this.state.check=0, /*DataOganizer:Component(this.state.bet),*/ bet: this.state.bet=0, /*DataOganizer:Component(this.state.call),*/
     call: this.state.call=0, /*DataOganizer:Comment(this.state.raise),*/  raise: this.state.raise=0, /*reRaise: DataOganizer(this.state.reRaise),*/ reRaise: this.state.reRaise=0, /*fold: DataOganizer(this.state.fold), */fold: this.state.fold=0})
    } //Clears data after each hand, needs to also be sent to DataOganizer, so may need to change this.
   }
   //}
  //}
+
  render(){
    return (
        <View  style={{justifyContent: 'center', flex: 1,}}>
@@ -58,6 +89,16 @@ constructor(props) {
             </TouchableHighlight>
             <Text>   Hand #{this.state.handNumber}  </Text>
          </View>
+          <View style={{ flex: 0, alignItems: 'center', justifyContent: 'center' }}>     
+        <Switch
+           title="Blind"
+          onValueChange={this._handleToggleSwitch}
+          value={this.state.isBigBlind}
+        />
+        <Text> Are you Big Blind </Text>
+      </View> 
+
+
          <View style={{flex:2, backgroundColor:'yellow'}}>
            <View style={[styles.row1, styles.rows]}>
               <TouchableHighlight onPress={() => this.onPress('check')} style={[styles.button]}>
